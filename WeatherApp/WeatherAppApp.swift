3ab5bff451a7dad8164f13538875b8f54b9771f0
Berkin KOCA <bekoca@innova.com.tr>
2024-03-06 10:21:45 +0300

    First Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Berkin KOCA on 6.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
