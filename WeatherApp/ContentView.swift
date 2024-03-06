7a1b2959917068d4ab9e5b3b63a412c68a70e0c6
Berkin KOCA <bekoca@innova.com.tr>
2024-03-06 11:51:23 +0300

    Main View designed and dailyWeather View added.

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,13 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .lightBlue]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            VStack {
+                Text("Seattle, WA")
+                    .font(.system(size: 32, weight: .medium, design: .serif))
+                    .foregroundStyle(.white)
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 12)
+
+                VStack(spacing: 12) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                        
+                    Text("30°")
+                        .font(.system(size: 72, weight: .medium))
+                        .foregroundStyle(.white )
+                }
+                
+                HStack(spacing: 20) {
+                    dailyWeatherPreview(day: "Tue",
+                                        weatherImage: "cloud.sun.fill",
+                                        degree: 16)
+                    
+                    dailyWeatherPreview(day: "Wed",
+                                        weatherImage: "sun.max.fill",
+                                        degree: 20)
+                    
+                    dailyWeatherPreview(day: "THU",
+                                        weatherImage: "wind",
+                                        degree: 12)
+                    
+                    dailyWeatherPreview(day: "FRI",
+                                        weatherImage: "sunset.fill",
+                                        degree: 15)
+                    
+                    dailyWeatherPreview(day: "SAT",
+                                        weatherImage: "cloud.sun.fill",
+                                        degree: 22)
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 12)
+                
+                Spacer()
+
+            }
         }
-        .padding()
+    }
+}
+
+@ViewBuilder func dailyWeatherPreview(day: String, weatherImage: String, degree: Int) -> some View {
+    VStack {
+        Text(day)
+            .font(.system(size: 28, weight: .medium))
+            .foregroundStyle(.white)
+        
+        Image(systemName: weatherImage)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 52, height: 52)
+        
+        Text("\(degree)°")
+            .font(.system(size: 28, weight: .medium))
+            .foregroundStyle(.white)
     }
 }
 
